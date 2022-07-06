2cc0ee4dfc3115412b8b86b8e80b618d9ab7ef06
Henrique Albuquerque <ehf.albuquerque@gmail.com>
2022-07-05 21:56:04 -0300

    ✨ Adding Redux to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from 'store';
+
 import App from 'pages/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
