1fa566cdc49096eb2f5791f02e176f60ca4ad5eb
Henrique Albuquerque <ehf.albuquerque@gmail.com>
2022-07-12 21:37:19 -0300

    🎨 Improving Start Page

--- src/pages/Start/index.tsx
+++ src/pages/Start/index.tsx
@@ -1,39 +1,87 @@
-import React, { FormEvent, useState } from 'react';
-import * as S from './styles';
+import React, { useRef, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+
+import { useDispatch } from 'react-redux';
+import { enterRoom } from 'store/Game.store';
 
 import Modal from 'components/Modal';
 import Button from 'components/Button';
-import InputText from 'components/InputText';
+import { InputWithButton } from 'components/Input';
+import { Form } from '@unform/web';
+
+interface RedirectPage {
+  state: boolean;
+  to: string;
+}
+
+interface Errors {
+  [key: string]: string;
+}
+
+interface Data {
+  room: string;
+}
 
 const Start = () => {
-  const [room, setRoom] = useState<number>(0);
+  const dispatch = useDispatch();
+  const formRef = useRef<FormHandles>(null);
 
-  const handleChange = (e: FormEvent<HTMLInputElement>): void => {
-    const room = Number(e.currentTarget.value);
+  const [redirect, setRedirect] = useState<RedirectPage>({
+    state: false,
+    to: ''
+  });
 
-    room <= 99999 && room >= 10000 ? setRoom(room) : setRoom(0);
-  };
+  async function handleSubmit(data: Data) {
+    try {
+      const schema = Yup.object().shape({
+        room: Yup.number().required('O campo é obrigatório')
+      });
 
-  const handleURL = (room: number): string => {
-    return room !== 0 ? `../room/${room}/player` : '/';
-  };
+      await schema.validate(data, { abortEarly: false });
 
-  return (
-    <Modal title="Cards against humanity." subtitle="by RickyAlbuq">
-      <Button type="cta" toGo="player" label="Jogar" />
-      <Button type="cta" toGo="create" label="Criar sala" />
-      <S.Wrapper>
-        <InputText
-          onChange={handleChange}
-          label="Ou entre em uma sala:"
-          placeholder="00000"
-          type="number"
-        />
-        <Button type="cta" toGo={handleURL(room)} label="Entrar" />
-      </S.Wrapper>
-      <Button type="cta" toGo="rules" label="Regras &#38; Sugestões" />
-    </Modal>
-  );
+      dispatch(enterRoom({ room: Number(data.room) }));
+
+      setRedirect({
+        state: true,
+        to: `../room/${data.room}`
+      });
+    } catch (err) {
+      const validationErrors: Errors = {};
+
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          if (error.path) {
+            validationErrors[error.path] = error.message;
+          }
+        });
+        formRef?.current?.setErrors(validationErrors);
+      }
+    }
+  }
+
+  if (redirect.state) {
+    return <Navigate to={redirect.to} />;
+  } else {
+    return (
+      <Modal title="Cards against humanity." subtitle="by RickyAlbuq">
+        <Button type="cta" toGo="choose" label="Jogar" />
+        <Button type="cta" toGo="create" label="Criar sala" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <InputWithButton
+            name="room"
+            type="number"
+            placeholder="00000"
+            btnLabel="Entrar"
+            label="Ou entre em uma sala:"
+          />
+        </Form>
+        <Button type="cta" toGo="rules" label="Regras &#38; Sugestões" />
+      </Modal>
+    );
+  }
 };
 
 export default Start;
--- src/pages/Start/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  display: flex;
-  gap: ${({ theme }) => theme.spacings.sm};
-  align-items: flex-end;
-`;
