8bf202626c046233ae74c8e2aecba4e5b58e9ee9
Henrique Albuquerque <ehf.albuquerque@gmail.com>
2022-07-12 21:20:23 -0300

    🎨 Improving Button Component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import * as S from './styles';
 
 interface Props {
-  toGo: string;
+  toGo?: string;
   label: string;
-  type: 'cta' | 'ghost';
+  type: 'cta' | 'ghost' | 'form';
+  onClick?: MouseEventHandler;
 }
 
-const Button: React.FC<Props> = ({ toGo, label, type }) => {
+const Button: React.FC<Props> = ({ toGo, label, type, onClick }) => {
   switch (type) {
     case 'cta':
-      return <S.Cta to={toGo}>{label}</S.Cta>;
+      return <S.Cta to={toGo || ''}>{label}</S.Cta>;
     case 'ghost':
-      return <S.Ghost to={toGo}>{label}</S.Ghost>;
+      return (
+        <S.Ghost onClick={onClick} to={toGo || ''}>
+          {label}
+        </S.Ghost>
+      );
+    case 'form':
+      return (
+        <S.Form type="submit" onClick={onClick}>
+          {label}
+        </S.Form>
+      );
     default:
       return null;
   }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -30,3 +30,59 @@ export const Ghost = styled(Link)(
     }
   `
 );
+
+export const Form = styled.button(
+  ({ theme }) => css`
+    width: 100%;
+    padding: ${theme.spacings.sm};
+    border: none;
+    border-radius: ${theme.radius.sm};
+    background: ${theme.colors.bgMedium};
+    text-align: center;
+    color: ${theme.colors.txtMedium};
+    font-size: ${theme.fontSizes.sm};
+    line-height: ${theme.lineHeights.heading};
+    &:hover {
+      cursor: pointer;
+      background: ${theme.colors.bgHigh};
+    }
+  `
+);
+
+export const BtnGroupCenter = styled.button.attrs(() => ({
+  type: 'button'
+}))(
+  ({ theme }) => css`
+    width: 100%;
+    padding: ${theme.spacings.sm};
+    border: none;
+    border-radius: 0;
+    background: ${theme.colors.bgMedium};
+    text-align: center;
+    color: ${theme.colors.txtMedium};
+    font-size: ${theme.fontSizes.sm};
+    line-height: ${theme.lineHeights.heading};
+    &:hover {
+      cursor: pointer;
+      background: ${theme.colors.bgHigh};
+    }
+  `
+);
+
+export const BtnGroupLeft = styled(BtnGroupCenter)(
+  ({ theme }) => css`
+    border-radius: ${theme.radius.sm} 0 0 ${theme.radius.sm};
+  `
+);
+
+export const BtnGroupRight = styled(BtnGroupCenter)(
+  ({ theme }) => css`
+    border-radius: 0 ${theme.radius.sm} ${theme.radius.sm} 0;
+  `
+);
+
+export const BtnGroupWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 2px;
+`;
